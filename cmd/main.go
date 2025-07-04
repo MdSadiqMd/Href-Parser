1f0bd6658a1be83a6f181bdc721b732984803617
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-07-05 00:58:55 +0530

    up and running

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/MdSadiqMd/Href-Parser/pkg"
+)
+
+var exampleHtml = `
+<html>
+<body>
+  <h1>Hello!</h1>
+  <a href="/other-page">
+    A link to another page
+    <span> some span  </span>
+  </a>
+  <a href="/page-two">A link to a second page</a>
+</body>
+</html>
+`
 
 func main() {
-	fmt.Println("Hello world")
+	r := strings.NewReader(exampleHtml)
+	links, err := pkg.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MdSadiqMd/Href-Parser
 
 go 1.23.3
+
+require golang.org/x/net v0.41.0
